9f840d012c2fa8e5a0f6d90cdae3e3e90028a1ec
Adrian <majcherrr87@gmail.com>
2025-01-19 23:50:46 +0100

    add table

--- src/pages/AdminBooks/BooksList.tsx
+++ src/pages/AdminBooks/BooksList.tsx
@@ -9,82 +9,87 @@ import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { booksOptions } from "../../queries/books";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { Book } from "../../types/index";
 
 interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
+  id: keyof Data;
   label: string;
   minWidth?: number;
-  align?: "right";
-  format?: (value: number) => string;
+  align?: "right" | "center";
 }
 
 const columns: readonly Column[] = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  { id: "title", label: "Tytuł", minWidth: 170 },
+  { id: "author", label: "Autor", minWidth: 100 },
   {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
+    id: "year",
+    label: "Rok",
     minWidth: 50,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
+    align: "center",
   },
   {
-    id: "density",
-    label: "Density",
+    id: "availableCopies",
+    label: "Dos.",
+    minWidth: 50,
+    align: "center",
+  },
+  {
+    id: "borrowedCopies",
+    label: "Wyp.",
+    minWidth: 50,
+    align: "center",
+  },
+  {
+    id: "allBooks",
+    label: "Wszystkie",
+    minWidth: 50,
+    align: "center",
+  },
+  {
+    id: "img",
+    label: "Foto",
+    minWidth: 50,
+    align: "center",
+  },
+  {
+    id: "action",
+    label: "Akcje",
     minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
+    align: "center",
   },
 ];
 
 interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  id: string;
+  title: string;
+  author: string;
+  year: number;
+  availableCopies: number;
+  borrowedCopies: number;
+  allBooks: number;
+  img: boolean;
+  action: string;
 }
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+const createData = (data: Book): Data => {
+  return {
+    ...data,
+    action: "kot",
+    allBooks: data.availableCopies + data.borrowedCopies,
+    img: !!data.images,
+  };
+};
 
 export const BooksList = () => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const { data } = useSuspenseQuery(booksOptions);
+
+  const books: Data[] = [];
+  data.forEach((book) => {
+    return books.push(createData(book));
+  });
 
   const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
@@ -115,17 +120,19 @@ export const BooksList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {books
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
+                          {typeof value === "boolean"
+                            ? value
+                              ? "Tak"
+                              : "Nie"
                             : value}
                         </TableCell>
                       );
@@ -139,7 +146,7 @@ export const BooksList = () => {
       <TablePagination
         rowsPerPageOptions={[5, 10, 25, 50]}
         component="div"
-        count={rows.length}
+        count={data.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
