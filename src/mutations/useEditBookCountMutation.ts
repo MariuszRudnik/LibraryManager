7a0d7dd4c9da0629986a498eee6b460cc34a9c4b
Adrian <majcherrr87@gmail.com>
2025-01-24 20:30:57 +0100

    edit book

---
+++ src/mutations/useEditBookCountMutation.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { apiCall } from "../utills/apiCall";
+import { Book } from "../types";
+
+export const useEditBookCountMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["update-book"],
+    mutationFn: async (body: Book) => {
+      return await apiCall(`books/${body.id}`, {
+        method: "PUT",
+        body,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["books"],
+        exact: true,
+      });
+    },
+  });
+};
--- src/routes/dashboard/books.tsx
+++ src/routes/dashboard/books.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Books } from "../../pages/AdminBooks/Books";
+import { booksOptions } from "../../queries/books";
 
 export const Route = createFileRoute("/dashboard/books")({
   component: Books,
+  loader: async (data) => {
+    const { queryClient } = data.context;
+    return queryClient.ensureQueryData(booksOptions);
+  },
 });
