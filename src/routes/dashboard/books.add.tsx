85a60966f848e993109d92a98075081ebea245ec
Kamil <kamil.amb@onet.pl>
2025-01-25 20:47:01 +0100

    Style book.add

--- src/routes/dashboard/books.add.tsx
+++ src/routes/dashboard/books.add.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, Container } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
 import { useInput } from "../../hooks/useInput";
 import { FormEvent } from "react";
@@ -31,93 +31,130 @@ function RouteComponent() {
 
     console.log(addBook);
   };
+
   return (
-    <Box display="flex" flexDirection="row">
-      {/* <img src={urlInput.value} loading="lazy" /> */}
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "2rem 0",
+        backgroundColor: "#adaaaa",
+        minHeight: "calc(100vh - 96px)", // Zmniejszamy wysokość tła o 4% (2% z góry, 2% z dołu)
+      }}
+    >
       <Box
-        component="form"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          width: "100%",
+          maxWidth: "800px",
+          backgroundColor: "white",
+          borderRadius: "8px",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          padding: "2rem",
         }}
-        autoComplete="off"
-        onSubmit={handleSubmit}
       >
-        <Typography variant="h4" component="h1" sx={{ marginBottom: "1.5rem" }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ marginBottom: "1.5rem", fontWeight: "bold", textAlign: "center" }}
+        >
           Wprowadź książkę
         </Typography>
-        <TextField
-          required
-          id="title"
-          label="Tytuł"
-          type="text"
-          variant="outlined"
-          {...titleInput}
-          sx={{ marginBottom: "1rem" }}
-        />
-        <TextField
-          required
-          id="author"
-          label="Autor"
-          type="text"
-          variant="outlined"
-          {...authorInput}
-          sx={{ marginBottom: "1rem" }}
-        />
-        <Box display="flex" flexDirection="row">
-          <TextField
-            required
-            id="year"
-            label="Rok"
-            type="number"
-            variant="outlined"
-            {...yearInput}
-            sx={{ marginBottom: "1rem" }}
-          />
-          <TextField
-            required
-            id="countBook"
-            label="Ilość"
-            type="number"
-            variant="outlined"
-            {...countBookInput}
-            sx={{ marginBottom: "1rem" }}
-          />
-        </Box>
-        <TextField
-          required
-          id="description"
-          label="Opis"
-          type="text"
-          variant="outlined"
-          {...descriptionInput}
-          sx={{ marginBottom: "1rem" }}
-        />
-        <TextField
-          required
-          id="images"
-          label="URL"
-          type="url"
-          variant="outlined"
-          {...urlInput}
-          sx={{ marginBottom: "1rem" }}
-        />
 
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
           sx={{
-            marginBottom: "1rem",
-            width: "100%",
-            padding: "0.75rem",
-            fontSize: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          Zaloguj się
-        </Button>
+          <TextField
+            required
+            id="title"
+            label="Tytuł"
+            type="text"
+            variant="outlined"
+            {...titleInput}
+            sx={{ marginBottom: "1rem", width: "100%" }}
+          />
+          <TextField
+            required
+            id="author"
+            label="Autor"
+            type="text"
+            variant="outlined"
+            {...authorInput}
+            sx={{ marginBottom: "1rem", width: "100%" }}
+          />
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", sm: "row" },
+              gap: "1rem",
+              width: "100%",
+              marginBottom: "1rem",
+            }}
+          >
+            <TextField
+              required
+              id="year"
+              label="Rok"
+              type="number"
+              variant="outlined"
+              {...yearInput}
+              sx={{ flex: 1 }}
+            />
+            <TextField
+              required
+              id="countBook"
+              label="Ilość"
+              type="number"
+              variant="outlined"
+              {...countBookInput}
+              sx={{ flex: 1 }}
+            />
+          </Box>
+
+          <TextField
+            required
+            id="description"
+            label="Opis"
+            type="text"
+            variant="outlined"
+            multiline
+            rows={3}
+            {...descriptionInput}
+            sx={{ marginBottom: "1rem", width: "100%" }}
+          />
+          <TextField
+            required
+            id="images"
+            label="URL"
+            type="url"
+            variant="outlined"
+            {...urlInput}
+            sx={{ marginBottom: "1rem", width: "100%" }}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            sx={{
+              width: "100%",
+              padding: "0.75rem",
+              fontSize: "1rem",
+            }}
+          >
+            Dodaj książkę
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 }
